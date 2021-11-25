b6f7281042d95931404e7eacca41101d87e92545
YoAMIOT <yoann.amiot.dev@gmail.com>
2021-11-25 13:02:04 +0100

    up good looking scene

--- scripts/MapTest.gd
+++ scripts/MapTest.gd
@@ -10,7 +10,7 @@ const MAX_Z_POS : int = 1000;
 const MIN_X_POS : int = -1000;
 const MAX_X_POS : int = 1000;
 const PINE_Y_POS : float = 8.0;
-const BUSH_Y_POS : float = 2.905;
+const BUSH_Y_POS : float = 2.8;
 const PINE_NUMBER: int = 2000;
 const BUSH_NUMBER : int = 4000;
 
@@ -23,7 +23,7 @@ var Rng : RandomNumberGenerator = RandomNumberGenerator.new();
 func _ready():
 	for i in PINE_NUMBER:
 		var instance = Pine.instance();
-		add_child(instance);
+		get_parent().get_node("Pines").add_child(instance);
 		Rng.randomize();
 		var instanceXPos = Rng.randf_range(MIN_X_POS, MAX_X_POS);
 		Rng.randomize();
@@ -32,9 +32,11 @@ func _ready():
 
 	for j in BUSH_NUMBER:
 		var instance = Bush.instance();
-		add_child(instance);
+		get_parent().get_node("Bushes").add_child(instance);
 		Rng.randomize();
 		var instanceXPos = Rng.randf_range(MIN_X_POS, MAX_X_POS);
 		Rng.randomize();
 		var instanceZPos = Rng.randf_range(MIN_Z_POS, MAX_Z_POS);
 		instance.translation = Vector3(instanceXPos, BUSH_Y_POS, instanceZPos);
+
+	queue_free();
