f4391ea29bfb405e494f7e392dd4319025ed05bb
AMIOT Yoann <yoann.amiot.dev@gmail.com>
2021-12-08 22:03:37 +0100

    up Valid regex and error message

--- DeforestationPunk/scripts/MainMenu.gd
+++ DeforestationPunk/scripts/MainMenu.gd
@@ -6,7 +6,10 @@ var regex : RegEx = RegEx.new();
 
 ###Ready Function###
 func _ready():
-	regex.compile("^(25[0–5]|2[0–4][0–9]|[01]?[0–9][0–9]?).(25[0–5]|2[0–4][0–9]|[01]?[0–9][0–9]?).(25[0–5]|2[0–4][0–9]|[01]?[0–9][0–9]?).(25[0–5]|2[0–4][0–9]|[01]?[0–9][0–9]?)$");
+	regex.compile("\\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b");
+	#The RegEx is: \b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b
+
+
 
 ###Play###
 func _on_Play_pressed():
@@ -26,11 +29,18 @@ func _on_Back_pressed():
 func _on_JoinServer_pressed():
 	get_node("ServerMenu/Error").visible = false;
 	get_node("ServerMenu/Error/ErrorBackground/AdressEmpty").visible = false;
+	get_node("ServerMenu/Error/ErrorBackground/InvalidAddress").visible = false;
+
 	if get_node("ServerMenu/IpAddress").text.empty() == false:
-		Server.ip = get_node("ServerMenu/IpAddress").text;
-		Server.connectToServer();
+		var result = regex.search(get_node("ServerMenu/IpAddress").text);
+		if result:
+			Server.ip = get_node("ServerMenu/IpAddress").text;
+			Server.connectToServer();
 #		if get_tree().change_scene("res://scenes/Map.tscn") != OK:
 #			print ("An unexpected error occured when trying to switch to the Map scene")
+		else:
+			get_node("ServerMenu/Error").visible = true;
+			get_node("ServerMenu/Error/ErrorBackground/InvalidAddress").visible = true;
 	elif get_node("ServerMenu/IpAddress").text.empty() == true:
 		get_node("ServerMenu/Error").visible = true;
 		get_node("ServerMenu/Error/ErrorBackground/AdressEmpty").visible = true;
